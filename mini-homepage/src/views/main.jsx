0fbca32278e42e33724093fede8178f65a59261f
gahee <ogh010@naver.com>
2024-02-03 12:34:59 +0900

    feat axios 설치 및 css 진행중

--- mini-homepage/src/views/main.jsx
+++ mini-homepage/src/views/main.jsx
@@ -4,7 +4,22 @@ import React, { Component } from 'react'
 export default class Main extends Component {
   render() {
     return (
-      <div>메인입니다</div>
+      <div class='wrap'>
+        <div class='header'>
+          <div className=''>
+            <div className='logo'>logo</div>
+            <div className='loginBox'>
+              <div className='join'></div>
+              <div className='in'></div>
+              <div className='out'></div>
+            </div>
+          </div>
+        </div>
+        <div class='content'>
+          <div></div>
+        </div>
+        <div class='footer'></div>
+      </div>
     )
   }
 }
