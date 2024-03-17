42c12472407c318e9bb7393284e8aad4ecf110c0
bluej1996 <bluej1996@gmail.com>
2024-03-17 15:49:42 +0900

    클래스컴포넌트 함수컴포넌트로 변경

--- mini-homepage/src/services/index.js
+++ mini-homepage/src/services/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-
 const BASE_URL = 'http://localhost:5000';
 const TIMEOUT = 10 * 1000;
 
@@ -9,39 +7,38 @@ const TIMEOUT = 10 * 1000;
  * @see https://stack94.tistory.com/entry/TypeScript-Axios-TypeScript-%EC%A0%81%EC%9A%A9%ED%95%98%EC%97%AC-%EC%82%AC%EC%9A%A9%ED%95%B4%EB%B3%B4%EC%9E%90
  *  msw서버를 사용할 것인지, 실제 서버 호출할 것인지 선택가능할 듯
  */
-export default class HTTPProvider {
-  private client: AxiosInstance;
-
+class service {
   constructor() {
     this.client = axios.create({
       baseURL: BASE_URL,
       timeout: TIMEOUT,
       headers: { 'Content-Type': 'application/json' },
-      responseType: 'json' as const,
+      responseType: 'json',
     });
   }
 
-  get(path: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+  get(path, config) {
     return this.client.get(path, config);
   }
 
-  post(path: string, body?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+  post(path, body, config) {
     return this.client.post(path, body, config);
   }
 
-  postFormData(path: string, formData: FormData): Promise<AxiosResponse> {
+  postFormData(path, formData) {
     return this.client.postForm(path, formData);
   }
 
-  put(path: string, body?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+  put(path, body, config) {
     return this.client.put(path, body, config);
   }
 
-  patch(path: string, body?: unknown, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+  patch(path, body, config) {
     return this.client.patch(path, body, config);
   }
 
-  delete(path: string, config?: AxiosRequestConfig): Promise<AxiosResponse> {
+  delete(path, config) {
     return this.client.delete(path, config);
   }
 }
+export default service;
--- mini-homepage/src/views/profile.jsx
+++ mini-homepage/src/views/profile.jsx
@@ -1,50 +1,47 @@
 // import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="wrap">
-          <div className="profileContent">
-            <div className="flex g16 p36">
-              <div className="profileBox1">
-                <div className="box">
-                  <div className="profilePicture">사진</div>
-                  <div className="profileEditIconWrap">
-                    <i className="profileEditIcon"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="profileBox2">
-                <div className="box">
-                  <div className="profileContainer">
-                    <div className="profileSubConent">
-                      <span>사이트 제목</span>
-                      <input type="text" className="input" placeholder="아이슈의 미니홈피" />
-                    </div>
-                    <div className="profileSubConent">
-                      <span>별명</span>
-                      <input type="text" className="input" placeholder="아이슈" />
-                    </div>
-                    <div className="profileSubConent">
-                      <span>상태 메세지</span>
-                      <input type="text" className="input" placeholder="믿어 의심치 않아" />
-                    </div>
-                  </div>
-                </div>
-                <div className="profileBtnContainer">
-                  <div className="profileBtn">취소</div>
-                  <div className="profileBtn">확인</div>
+const profile = () => {
+  return (
+    <div className="container">
+      <div className="wrap">
+        <div className="profileContent">
+          <div className="flex g16 p36">
+            <div className="profileBox1">
+              <div className="box">
+                <div className="profilePicture">사진</div>
+                <div className="profileEditIconWrap">
+                  <i className="profileEditIcon"></i>
                 </div>
               </div>
             </div>
+            <div className="profileBox2">
+              <div className="box">
+                <div className="profileContainer">
+                  <div className="profileSubConent">
+                    <span>사이트 제목</span>
+                    <input type="text" className="input" placeholder="아이슈의 미니홈피" />
+                  </div>
+                  <div className="profileSubConent">
+                    <span>별명</span>
+                    <input type="text" className="input" placeholder="아이슈" />
+                  </div>
+                  <div className="profileSubConent">
+                    <span>상태 메세지</span>
+                    <input type="text" className="input" placeholder="믿어 의심치 않아" />
+                  </div>
+                </div>
+              </div>
+              <div className="profileBtnContainer">
+                <div className="profileBtn">취소</div>
+                <div className="profileBtn">확인</div>
+              </div>
+            </div>
           </div>
-          <div className="footer"></div>
         </div>
+        <div className="footer"></div>
       </div>
-    );
-  }
-}
-
-// const styles = StyleSheet.create({})
+    </div>
+  );
+};
+export default profile;
